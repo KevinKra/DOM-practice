fe6e3e3a44266bafbbeb7a6285c65d1f4d1b9462
Kevin Krato <kratok08@gmail.com>
2019-02-24 21:25:57 -0700

    Complete list appending

---
+++ scripts/script-answers.js
@@ -0,0 +1,11 @@
+
+
+
+// 2. Create a new ul element and populate it with 2 list elements.
+
+// const newList = document.createElement("ul");
+// const newLi = document.createElement("li");
+// const newLi2 = document.createElement("li");
+
+// newList.appendChild(newLi);
+// newList.appendChild(newLi2);
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,26 @@
 const mainArticle = document.querySelector(".main-article");
 const list = document.querySelector("ul");
 
+//2
+/* Create an unordered list, create two list elements
+place the second list element before the first list element.
+Append unordered list to mainArticle */  
+const newUl = document.createElement("ul");
 
-//CREATE A NEW UL LIST AND POPULATE IT
-const newList = document.createElement("ul");
 const newLi = document.createElement("li");
-const newLi2 = document.createElement("li");
+const newLiText = document.createTextNode("List one");
+newLi.appendChild(newLiText);
+newUl.appendChild(newLi);
 
-newList.appendChild(newLi);
-newList.appendChild(newLi2);
+const newLi2 = document.createElement("li");
+const newLiText2 = document.createTextNode("List two");
+newLi2.appendChild(newLiText2);
+newUl.insertBefore(newLi2, newUl.firstChild);
+
+mainArticle.appendChild(newUl);
+
+
+
 
 const liEl = document.getElementById("two");
 const paragraph = document.querySelector("p");
@@ -21,9 +33,6 @@ const liCollection = document.querySelectorAll("li[id]")
 mainArticle.firstChild.nextSibling.className = "green";
 liEl.className = "green";
 paragraph.innerHTML += " Hello!!";
-
-mainArticle.appendChild(newList);
-
 changeList();
 function changeList() {
   liCollection.forEach( element => {
